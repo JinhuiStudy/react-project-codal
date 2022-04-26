e1e55df51330686abc4296710677ac2199729f58
conny <jmg41490400@gmail.com>
2022-04-26 10:25:56 +0900

    add prettier config

--- packages/components/src/Button/button.stories.tsx
+++ packages/components/src/Button/button.stories.tsx
@@ -1,49 +1,49 @@
-import React from "react";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { Button } from "./button";
-import {userEvent, within} from "@storybook/testing-library";
-import {expect} from "@storybook/jest";
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Button } from './button';
+import { userEvent, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "SRC/Button",
+  title: 'SRC/Button',
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    backgroundColor: { control: "color" },
-    onClick: { action: "clicked" },
+    backgroundColor: { control: 'color' },
+    onClick: { action: 'clicked' },
   },
 } as ComponentMeta<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = args => <Button {...args} />;
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
-  label: "Button",
+  label: 'Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: "Button",
+  label: 'Button',
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  size: "large",
-  label: "Button",
+  size: 'large',
+  label: 'Button',
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: "small",
-  label: "Button",
+  size: 'small',
+  label: 'Button',
 };
 export const Test = Template.bind({});
-Test.play = async ({args,canvasElement}) => {
+Test.play = async ({ args, canvasElement }) => {
   const canvas = within(canvasElement);
   await userEvent.click(canvas.getByRole('button'));
   await expect(args.onClick).toHaveBeenCalled();
-}
+};
